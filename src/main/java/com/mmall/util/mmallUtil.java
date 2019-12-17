606998c444a4227eec4ef5cd34bfc5c6358bf5ee
sunhd <lazy@lazydeMacBook-Pro.local>
2019-12-17 13:41:03 +0800

    delete

--- src/main/java/com/mmall/util/mmallUtil.java
+++ src/main/java/com/mmall/util/mmallUtil.java
@@ -5,17 +5,11 @@ import java.util.Map;
 public class mmallUtil {
 
     public static boolean isNullorEmpty(Object object){
-        if(object instanceof String){
-            if(((String) object).length()==0 || object == ""){
+        if(object instanceof String) {
+            if (((String) object).length() == 0 || object == "") {
                 return true;
             }
         }
-        if(object instanceof Map){
-            if(((Map) object).size() == 0){
-                return true;
-            }
-        }
-
         return false;
     }
 }
